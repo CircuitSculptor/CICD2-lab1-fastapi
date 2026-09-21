9ba8317493d9c9ef2633440b018ba793c5556780
CircuitSculptor <148762178+CircuitSculptor@users.noreply.github.com>
2026-09-21 15:57:11 +0000

    Add starting letter G to student id

--- app/schemas.py
+++ app/schemas.py
@@ -7,7 +7,7 @@ class UserCreate(BaseModel):
     name: Annotated[str, StringConstraints(min_length=2, max_length=50)]
     email: EmailStr
     age: int = Field(gt=18, lt=120)
-    student_id: Annotated[str, StringConstraints(pattern=r"^\d{7}")]
+    student_id: Annotated[str, StringConstraints(pattern=r"^G\d{7}")]
 
-    
+
 
