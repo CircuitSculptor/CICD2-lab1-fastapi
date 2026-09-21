a732c032a145931828080b6cbc19e9076c0e284e
CircuitSculptor <148762178+CircuitSculptor@users.noreply.github.com>
2026-09-21 15:56:35 +0000

    Format user function

--- app/main.py
+++ app/main.py
@@ -9,11 +9,18 @@ users: list[UserCreate] = []
 def health():
     return{"status": "ok"}
 
+@app.get("/hello")
+def hello():
+    return{"message":"Hello from FastAPI"}
+
 
 @app.post("/api/users", status_code=status.HTTP_201_CREATED)
 def add_user(new_user: UserCreate):
     for existing_user in users:
         if existing_user.user_id == new_user.user_id:
-            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="This user already exists")
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT, 
+                detail="This user already exists"
+            )
     users.append(new_user)
     return new_user
