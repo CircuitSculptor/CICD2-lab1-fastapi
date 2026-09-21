ecb8b407a0d56ad7533bf2631d538ef4ba18a62f
CircuitSculptor <148762178+CircuitSculptor@users.noreply.github.com>
2026-09-21 15:45:55 +0000

    More fixes

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ def health():
 @app.post("/api/users", status_code=status.HTTP_201_CREATED)
 def add_user(new_user: UserCreate):
     for existing_user in users:
-        if existing_user.user_id == new_user_id:
+        if existing_user.user_id == new_user.user_id:
             raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="This user already exists")
     users.append(new_user)
     return new_user
