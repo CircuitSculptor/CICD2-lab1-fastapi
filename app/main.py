8cb8ff3df5c9c16d8777c365ce3bad80412a7756
CircuitSculptor <148762178+CircuitSculptor@users.noreply.github.com>
2026-09-21 16:17:02 +0000

    Added delete function

--- app/main.py
+++ app/main.py
@@ -24,3 +24,14 @@ def add_user(new_user: UserCreate):
             )
     users.append(new_user)
     return new_user
+
+@app.delete("/api/users/{user_id}]", status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(user_id: int):
+    for index, existing_user in enumerate(users):
+        if existing_user.user_id == user_id:
+            users.pop(index)
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+    raise HTTPExcpetion(
+        status_cde=status.HTTP_404_NOT_FOUND, 
+        detail="User not found"
+    )
